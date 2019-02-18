6f4bfde01b0de732b4fc5384f7cda10dab4b9ccd
wolasoft <dev.wolasoft@gmail.com>
2019-02-18 22:50:09 +0100

    feat: create custom java library to tell jokes

---
+++ jokes_lib/src/main/java/com/wolasoft/jokes_lib/Joke.java
@@ -0,0 +1,11 @@
+package com.wolasoft.jokes_lib;
+
+public class Joke {
+    public Joke() {
+    }
+
+    public String sayJoke() {
+        String joke = "I was born in California. Which part? All of me.";
+        return joke;
+    }
+}

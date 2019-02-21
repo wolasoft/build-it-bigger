46b6a1417820829df324b28525c4293a2d33d72a
wolasoft <dev.wolasoft@gmail.com>
2019-02-21 04:54:40 +0100

    feat: add joke method to backend (gce) module

--- backend/src/main/java/com/udacity/gradle/builditbigger/backend/MyEndpoint.java
+++ backend/src/main/java/com/udacity/gradle/builditbigger/backend/MyEndpoint.java
@@ -3,6 +3,7 @@ package com.udacity.gradle.builditbigger.backend;
 import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
 import com.google.api.server.spi.config.ApiNamespace;
+import com.wolasoft.jokes_lib.Joke;
 
 import javax.inject.Named;
 
@@ -22,7 +23,17 @@ public class MyEndpoint {
     @ApiMethod(name = "sayHi")
     public MyBean sayHi(@Named("name") String name) {
         MyBean response = new MyBean();
-        response.setData("Hi, " + name);
+        response.setData("But, " + name);
+
+        return response;
+    }
+
+    /** A simple endpoint method that tells joke */
+    @ApiMethod(name = "sayJoke")
+    public MyBean sayJoke() {
+        Joke joke = new Joke();
+        MyBean response = new MyBean();
+        response.setData(joke.sayJoke());
 
         return response;
     }
